9eb659c940feea9b74feaaeacb623e700162107b
jk1dd <portajonk@gmail.com>
2017-07-27 15:03:23 -0600

    fix link typo to Link

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::LinksController < ApplicationController
   skip_before_action :verify_authenticity_token
-  
+
   def index
     @links = Link.all
   end
 
   def create
-    @link = link.new(link_params)
+    @link = Link.new(link_params)
     if @link.save
       head 201
     else
@@ -17,6 +17,6 @@ class Api::V1::LinksController < ApplicationController
   private
 
   def link_params
-    params.permit(:url)
+    params.require(:link).permit(:url)
   end
 end
